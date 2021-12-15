d29b38f2f14e93a40a9d218218661413a36e70b3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-12-15 16:57:38 -0500

    [+] Button: Stories usando Templates & Args

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -11,3 +11,31 @@ export const Primary = () => <Button variant='primary'>Primary</Button>;
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>;
 export const Success = () => <Button variant='success'>Success</Button>;
 export const Danger = () => <Button variant='danger'>Danger</Button>;
+
+// Crea una plantilla para evitar la repeticion del cuerpo del componente
+const Template = args => <Button { ...args } />;        
+
+// Stories: Variaciones a partir de una plantilla. Args representan el estado del componente en cada historia
+export const PrimaryButton = Template.bind({});
+PrimaryButton.args = {
+    variant: 'primary',
+    children: 'Primary Args'
+}
+
+export const SecondaryButton = Template.bind({});
+SecondaryButton.args = {
+    variant: 'secondary',
+    children: 'Secondary Args'
+}
+
+export const SuccessButton = Template.bind({});
+SuccessButton.args = {
+    variant: 'success',
+    children: 'Success Args'
+}
+
+export const DangerButton = Template.bind({});
+DangerButton.args = {
+    variant: 'danger',
+    children: 'Danger Args'
+}
