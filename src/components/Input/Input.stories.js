300dff9097f7e3b4de177bfde1d90a349c962eb6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-12-15 16:24:11 -0500

    [+] Input (Small): Renombra Small story

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -10,3 +10,6 @@ export default {
 export const Small = () => <Input size="small" placeholder="Small size" />;
 export const Medium = () => <Input size="medium" placeholder="Medium size" />;
 export const Large = () => <Input size="large" placeholder="Large size" />;
+
+// Renombrar la Historia
+Small.storyName = 'Small input';
